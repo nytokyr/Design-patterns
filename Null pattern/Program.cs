b0bf53572a88927a80c77e0b965dc34820858eed
nytokyr <nyiharan@gmail.com>
2019-07-02 20:37:24 +0200

    Ajoutez des fichiers projet.

---
+++ Null pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Null_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
